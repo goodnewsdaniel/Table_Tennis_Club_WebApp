e3adbcd4115c154df2c3709fa329f7c16406c2ff
Goodnews Daniel <goodnewsdaniel101@gmail.com>
2023-06-09 16:28:00 +0100

    Renamed model members as Member

--- myapp/my_table_tennis_club/members/models.py
+++ myapp/my_table_tennis_club/members/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class members (models.Model):
+class Member (models.Model):
 
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
