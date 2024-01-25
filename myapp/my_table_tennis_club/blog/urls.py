705366baa43ec4237efb78b7f2d75d4ac30d54dc
Goodnews Daniel <goodnewsdaniel101@gmail.com>
2024-01-25 04:38:36 +0100

    Updated Blog app

--- myapp/my_table_tennis_club/blog/urls.py
+++ myapp/my_table_tennis_club/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.blog, name='blog'),
+    path('blog/', views.blog, name='blog'),
     path('blog/details/<slug:slug>', views.details, name='details'),
 
 ]
